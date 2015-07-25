c50ca22fa9450c128fdfd912822b6e6a9f89ce25
suhas <suphya4u@gmail.com>
2015-07-24 20:10:43 -0700

    Working end-to-end locally. Demo app engine + android app (not much changed from sample app). https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/GcmEndpoints
    AppEngine
    - RegistrationApi -> register, listRegisteredDevices, unregister
    - MessagingApi -> sendMessage to all registered devices.
    
    Android
    - Registers itself when app opens
    - Receives broadcast messages from server and shows toast.

---
+++ server/src/main/java/com/letsmeet/server/RegistrationRecord.java
@@ -0,0 +1,52 @@
+package com.letsmeet.server;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+/**
+ * The Objectify object model for device registrations we are persisting
+ */
+@Entity
+public class RegistrationRecord {
+
+  @Id
+  Long id;
+
+  @Index
+  private String regId;
+
+  private String phoneNumber;
+
+  private String name;
+
+  public RegistrationRecord() {
+  }
+
+  public String getRegId() {
+    return regId;
+  }
+
+  public RegistrationRecord setRegId(String regId) {
+    this.regId = regId;
+    return this;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public RegistrationRecord setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+    return this;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public RegistrationRecord setName(String name) {
+    this.name = name;
+    return this;
+  }
+}
