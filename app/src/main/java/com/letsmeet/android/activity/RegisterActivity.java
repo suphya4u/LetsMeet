5c62cd7e8aa39c288b0a23fa8851f879a6932e14
suhas <suphya4u@gmail.com>
2015-08-15 14:19:41 -0700

    Move AsyncTask changes to RegistrationActivity from GcmRegistration. So that sms verification can be handled with Gcm registration and our user registration.

--- app/src/main/java/com/letsmeet/android/activity/RegisterActivity.java
+++ app/src/main/java/com/letsmeet/android/activity/RegisterActivity.java
@@ -1,6 +1,9 @@
 package com.letsmeet.android.activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.letsmeet.com.letsmeet.R;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -10,8 +13,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.letsmeet.android.apiclient.RegistrationServiceClient;
 import com.letsmeet.android.common.PhoneNumberHelper;
-import com.letsmeet.android.gcm.GcmRegistrationAsyncTask;
+import com.letsmeet.android.gcm.GcmRegistrationHandler;
+import com.letsmeet.android.storage.LocalStore;
+import com.letsmeet.server.registration.model.RegistrationRequest;
+import com.letsmeet.server.registration.model.RegistrationResponse;
+
+import java.io.IOException;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -29,9 +38,7 @@ public class RegisterActivity extends AppCompatActivity {
         String name = nameEditText.getText().toString();
         PhoneNumberHelper phoneNumberHelper = new PhoneNumberHelper(RegisterActivity.this);
         String phone = phoneNumberHelper.formatPhoneNumber(phoneEditText.getText().toString());
-        if (isInputValid(name, phone)) {
-          new GcmRegistrationAsyncTask(RegisterActivity.this, name, phone).execute();
-        }
+        registerUser(name, phone);
       }
     });
   }
@@ -58,7 +65,7 @@ public class RegisterActivity extends AppCompatActivity {
     return super.onOptionsItemSelected(item);
   }
 
-  public boolean isInputValid(String name, String phone) {
+  private boolean isInputValid(String name, String phone) {
     if (name.equals("")) {
       Toast.makeText(this, "Please enter name", Toast.LENGTH_LONG).show();
       return false;
@@ -68,5 +75,45 @@ public class RegisterActivity extends AppCompatActivity {
       return false;
     }
     return true;
+  }
+
+  private void registerUser(String name, String phone) {
+    final ProgressDialog pd = new ProgressDialog(this);
+
+    final RegistrationRequest registrationRequest = new RegistrationRequest()
+        .setName(name)
+        .setPhoneNumber(phone);
+
+    new AsyncTask<RegistrationRequest, Void, RegistrationResponse>() {
+
+      @Override
+      protected void onPreExecute() {
+        pd.show();
+      }
+
+      @Override protected RegistrationResponse doInBackground(RegistrationRequest... params) {
+        try {
+          String registrationId = GcmRegistrationHandler.getRegistrationId(RegisterActivity.this);
+          registrationRequest.setRegId(registrationId);
+          // TODO(suhas): Verify phone number by sms before registration.
+          RegistrationResponse response = RegistrationServiceClient.getInstance()
+              .registerUser(registrationRequest);
+          LocalStore localStore = LocalStore.getInstance(RegisterActivity.this);
+          localStore.saveUserId(response.getUserId());
+          return response;
+        } catch (IOException e) {
+          // TODO(suhas): Handle gracefully.
+          throw new RuntimeException(e);
+        }
+      }
+
+      @Override
+      protected void onPostExecute(RegistrationResponse response) {
+        pd.hide();
+        Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+      }
+    }.execute(registrationRequest);
   }
 }
---
+++ app/src/main/java/com/letsmeet/android/apiclient/RegistrationServiceClient.java
@@ -0,0 +1,62 @@
+package com.letsmeet.android.apiclient;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.letsmeet.android.config.Config;
+import com.letsmeet.server.registration.Registration;
+import com.letsmeet.server.registration.model.RegistrationRequest;
+import com.letsmeet.server.registration.model.RegistrationResponse;
+
+import java.io.IOException;
+
+/**
+ * Client wrapper for registration service.
+ */
+public class RegistrationServiceClient {
+
+  private static RegistrationServiceClient selfInstance;
+  private static Registration registrationService;
+
+  private RegistrationServiceClient() {};
+
+  public static RegistrationServiceClient getInstance() {
+    if (selfInstance == null) {
+      selfInstance = new RegistrationServiceClient();
+    }
+    return selfInstance;
+  }
+
+  public RegistrationResponse registerUser(RegistrationRequest request) {
+    try {
+      return getService().register(request).execute();
+    } catch (IOException e) {
+      // TODO(suhas) Handle gracefully?
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Registration getService() {
+    if (registrationService != null) {
+      return registrationService;
+    }
+
+    // TODO(suhas): Common code for all services. Move out.
+    Registration.Builder serviceBuilder = new Registration.Builder(
+        AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+        .setRootUrl(Config.getServerUrl());
+
+    if (Config.isEmulator()) {
+      serviceBuilder.setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+        @Override
+        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
+            throws IOException {
+          abstractGoogleClientRequest.setDisableGZipContent(true);
+        }
+      });
+    }
+    registrationService = serviceBuilder.build();
+    return registrationService;
+  }
+}
